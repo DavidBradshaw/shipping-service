a4356636ff8d907c34cc3623ff69336240ac7e54
tauber <tauber@at.ibm.com>
2018-08-29 13:35:42 +0200

    product service checks invalid responses

--- src/controller/shipping-controller.js
+++ src/controller/shipping-controller.js
@@ -3,9 +3,13 @@ function ShippingController() {}
 
 const REGULAR_PRICE = 0.1, OVERNIGHT_PRICE = 1
 
-ShippingController.prototype.getItemShipping = async function(item) {
-  var shippingAmount = await productService.getProductWeight(item.id)
-  return shippingAmount * REGULAR_PRICE
-}
+ShippingController.prototype.getItemShipping = async function (item) {
+    var shippingAmount = await productService.getProductWeight(item.id)
+    if (item.type.toLowerCase() === 'overnight') {
+      return shippingAmount * OVERNIGHT_PRICE
+    } else {
+      return shippingAmount * REGULAR_PRICE
+    }
+  }
 
 module.exports = ShippingController;

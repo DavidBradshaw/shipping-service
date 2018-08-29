dd3a332123d84898ac456ca2cd08364e754d10e4
tauber <tauber@at.ibm.com>
2018-08-29 11:53:28 +0200

    first test and shipping controller created

---
+++ tests/shipping-controller.test.js
@@ -0,0 +1,13 @@
+var chai = require('chai')
+var expect = chai.expect
+var ShippingController = require('../src/controllers/shipping-controller')
+
+describe('Shipping controller', function () {
+  var shippingCtrl = new ShippingController()
+
+  it('Should calculate correct shipping ', async function () {
+    let shipping = await shippingCtrl.getItemShipping({ id: 1, type: 'standard' })
+    expect(shipping).to.equal(0.5)
+  })
+
+})
